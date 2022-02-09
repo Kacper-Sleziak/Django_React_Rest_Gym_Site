83dd575dfa3308507b40f3ba127b3e6b534185a9
Kacper <kacper-sleziak@wp.pl>
2022-02-09 21:12:39 +0100

    Initial Commit

---
+++ src/account/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.password_validation import validate_password
+
+class AccountManager(BaseUserManager):
+    
+    def create_user(self, email, nickname, first_name, last_name, password=None):
+        if not email:
+            raise ValueError("User must have an email")        
+        
+        if not nickname:
+            raise ValueError("User must have an nickname")
+        
+        if not password:
+            raise ValueError("User must have password!")
+        
+        if not first_name:
+            raise ValueError("User must have first name!")
+
+        if not last_name:
+            raise ValueError("User must have last name!")
+
+        if validate_password(password) == None:
+        
+            user = self.model(
+                email = self.normalize_email(email),
+                nickname = nickname,
+                last_name = last_name,
+                first_name = first_name
+            )
+            
+            user.set_password(password)
+            user.save(using=self._db)    
+            
+            return user
+        
+    def create_superuser(self, email, nickname, first_name, last_name, password):
+        
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            nickname=nickname,
+            last_name = last_name,
+            first_name = first_name
+        )
+        
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    email                    = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    nickname                 = models.CharField(verbose_name="nick", max_length=30, unique=True)
+    first_name               = models.CharField(verbose_name="first_name", max_length=30, unique=False)
+    last_name                = models.CharField(verbose_name="last_name", max_length=30, unique=False)
+    date_joined              = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login               = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin                 = models.BooleanField(default=False)
+    is_active                = models.BooleanField(default=True)
+    is_staff                 = models.BooleanField(default=False)
+    is_superuser             = models.BooleanField(default=False)
+     
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname', 'first_name', 'last_name']
+
+    objects = AccountManager()
+
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def full_name(self):
+        full_name = str(self.nickname + " " + self.last_name)
+        return full_name
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
---
+++ src/blog/models.py
@@ -0,0 +1,70 @@
+from enum import unique
+from random import choices
+from django.db import models
+from django.db.models.signals import pre_save, pre_delete
+from django.utils.text import slugify
+from django.conf import settings
+from django.dispatch import receiver
+
+
+TAG_CHOICES = (
+    ('SPORT', 'SPORT'),
+    ('DIET', 'DIET'),
+    ('HEALTH', 'HEALTH'),
+    ('LIFESTYLE', 'LIFESTYLE'),
+)
+
+def image_upload_location(instance, filename):
+    file_path = f"blog/blog_images/{instance.author.email}/{instance.title}/{filename}"
+    
+    return file_path
+
+# Model Of Blog Post
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    body = models.TextField(max_length=5000, null=False, blank=False)
+    tag = models.CharField(max_length=15, choices=TAG_CHOICES, null=False, blank=False, default='SPORT')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, blank=False, on_delete=models.CASCADE)
+    released_date = models.DateTimeField(auto_now_add = True)
+    last_update = models.DateField(auto_now = True)
+    slug = models.SlugField(max_length=50, blank=True)
+    image = models.ImageField(upload_to=image_upload_location, blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
+
+# Handling slug creating
+@receiver(pre_save, sender=BlogPost)
+def slug_adder(sender, instance, *args, **kwargs):
+    unique_number = 2
+    slug = slugify(instance.title)
+    
+    # Checking if in data base is blog post with same slug
+    queryset = BlogPost.objects.all().filter(slug=slug)
+    
+    # Change slug if same exsits in data base
+    if queryset.exists():
+        while queryset.exists():
+            slug = slugify(instance.title + unique_number)
+            queryset = BlogPost.objects.all().filter(slug=slug)
+            unique_number += 1
+    
+    instance.slug = slug
+    
+@receiver(pre_delete, sender=BlogPost)
+def delete_image(sender, instance, *args, **kwargs):
+    try:
+        instance.image.delete(save=False)
+    except:
+        pass
+
+    
+
+
+        
+        
+        
+    
+    
+    
+    
